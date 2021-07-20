c4fc26ba2e0af19b39b3a9a12534ceb463850dff
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2021-07-20 13:56:58 +0300

    Modified the solution

--- 7-kyo-jaden-casing-strings.php
+++ 7-kyo-jaden-casing-strings.php
@@ -6,6 +6,7 @@ Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 
 Solutions: -->
+<?php
 function toJadenCase($string) 
 {
    return ucwords($string);
