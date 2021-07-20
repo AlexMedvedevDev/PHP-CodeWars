f537123c7378979faf185305adb55911908ba873
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2021-07-20 09:40:30 +0300

    Solution was added

--- 8-kyo-drink-about.php
+++ 8-kyo-drink-about.php
@@ -30,7 +30,7 @@ SOLUTIONS
 
 function people_with_age_drink(int $n): string {
     return 'drink ' . ($n < 21 ? $n < 18 ? $n < 14 ? 'toddy' : 'coke' : 'beer' : 'whisky');
-}
+} //is thiss right?
 
 // function people_with_age_drink(int $old): string {
 //     switch (true) {
