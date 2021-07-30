bdeca64f55deb35ca4cbdebadd5452c105af88f9
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2021-07-30 19:44:28 +0300

    solutions added

---
+++ 8-kyu-dna-to-rna-conversion.php
@@ -0,0 +1,30 @@
+INSTRICTIONS
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. 
+It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. 
+RNA differs slightly from DNA its chemical structure and contains no Thymine. 
+In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+Create a function which translates a given DNA string into RNA.
+For example:
+"GCAT"  =>  "GCAU"
+The input string can be of arbitrary length - in particular, it may be empty. 
+All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+SOLUTIONS
+<?php
+// function dnaToRna($str) {
+//     $dnaRegex = "/^(G|C|A|T)*$/i";
+//     if (preg_match($dnaRegex, $str)) {
+//         return strtoupper(str_replace("T", "U", $str));
+//     } 
+// }
+
+function dnaToRna($str) {
+    return str_replace("T", "U", $str);
+}
+
+// function dnaToRna($s) {
+//     $trans = array("T" => "U");
+//     return  strtr($s, $trans);      
+// }
