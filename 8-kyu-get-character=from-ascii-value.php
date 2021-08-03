74a2a5026fbf33593feda2c6a9ed72d903620ae8
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2021-08-03 14:33:51 +0300

    Solutions added

---
+++ 8-kyu-get-character=from-ascii-value.php
@@ -0,0 +1,16 @@
+INSTRUCTIONS
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+get_char(65) # => 'A'
+For ASCII table, you can refer to http://www.asciitable.com/
+
+SOLUTIONS
+<?php
+// function getChar($c)
+// {
+//   return chr($c);
+// }
+
+use function chr as getChar;
