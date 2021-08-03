29eee4d2fc8f689b85b2bf6ff6c9f3db3627eef5
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2021-08-03 18:06:41 +0300

    Solutions added

---
+++ 8-kyu-parse-nice-int-from-char-probler.php
@@ -0,0 +1,16 @@
+INSTRUCTIONS
+Ask a small girl - "How old are you?". She always says strange things... Lets help her!
+For correct answer program should return int from 0 to 9.
+Assume test input string always valid and may look like "1 year old" or "5 years old", etc.. The first char is number only.
+
+SOLUTIONS
+<?php
+use function intval as getAge;
+
+// function getAge($response) {
+//     return (int)$response{0};
+// }
+
+// function getAge($response) {
+//     return $response[0];
+// }
