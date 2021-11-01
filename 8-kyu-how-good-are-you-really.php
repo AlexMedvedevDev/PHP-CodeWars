71ab396c0bd3462e8a3a94e798784274b1e741d2
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2021-11-01 12:18:07 +0200

    Solutions added

---
+++ 8-kyu-how-good-are-you-really.php
@@ -0,0 +1,22 @@
+INSTRUCTIONS
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. 
+You want to know if you're better than the average student in your class.
+You receive an array with your peers' test scores. 
+Now calculate the average and compare your score!
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. 
+For calculating the average point you may add your point to the given array!
+
+SOLUTIONS
+<?php
+// function betterThanAverage($classPoints, $yourPoints) {
+//     $average = (array_sum($classPoints) + $yourPoints) / (count($classPoints) + 1);
+//     return $average < $yourPoints ? true : false;
+// }
+
+function betterThanAverage($classPoints, $yourPoints) {
+    return array_sum($classPoints) / count($classPoints) < $yourPoints;
+}
