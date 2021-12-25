464950fdb572c1f164cff4c7bdf78576059a9a8f
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2021-12-25 22:52:05 +0200

    Solutions added

---
+++ 7-kyu/how-many-urinals-are-free.php
@@ -0,0 +1,48 @@
+INSTRUCTIONS
+In men's public toilets with urinals, there is this unwritten rule that you leave at least one urinal free between you and the next person peeing. For example if there are 3 urinals and one person is already peeing in the left one, you will choose the urinal on the right and not the one in the middle. That means that a maximum of 3 people can pee at the same time on public toilets with 5 urinals when following this rule (Only 2 if the first person pees into urinal 2 or 4).
+Your task:
+You need to write a function that returns the maximum of free urinals as an integer according to the unwritten rule.
+Input
+A String containing 1s and 0s (Example: 10001) (1 <= Length <= 20)
+A one stands for a taken urinal and a zero for a free one.
+Examples
+10001 returns 1 (10101)
+1001 returns 0 (1001)
+00000 returns 3 (10101)
+0000 returns 2 (1001)
+01000 returns 1 (01010 or 01001)
+Note
+When there is already a mistake in the input string (for example 011), then return -1
+Have fun and don't pee into the wrong urinal
+
+SOLUTIONS
+<?php
+function GetFreeUrinals($urinals){
+    if(strpos($urinals, '11') !== false) return -1;
+    return preg_match_all('/(^|0)0(?!1)/', $urinals);
+}
+
+// function GetFreeUrinals($urinals){
+//     if (strstr($urinals, '11')) return -1;
+//     $count1 = 0;
+//     if (substr($urinals, 0, 2) == '00') {$count1++; $urinals[0] = '1';}
+//     $haystack = $urinals;
+//     $needle = '000';
+//     while (strpos($haystack, $needle) != false) {
+//       $count1++;
+//       $urinals = substr_replace($haystack, '010', strpos($haystack, $needle), strlen($needle));
+//       $haystack = $urinals;
+//     }
+//     if (substr($urinals, -2) == '00') {$count1++;};
+//     return $count1;
+// }
+
+// function GetFreeUrinals($urinals) {
+//     if (preg_match("/11/", $urinals)) {
+//       return -1;
+//     }
+//     preg_match_all("/(?<!1)0+(?!1)/", $urinals, $matches);
+//     return array_sum(array_map(function ($e) {
+//         return ceil(strlen($e) / 2);
+//       }, $matches[0]));
+// }
