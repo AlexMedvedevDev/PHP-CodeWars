670b6968ab418e291f325332b8d6a394a557796a
AlexMedvedevDev <alexmedvedev.amdev@gmail.com>
2022-07-29 18:47:56 +0300

    Solutions added

---
+++ heros-root.php
@@ -0,0 +1,50 @@
+INSTRUCTIONS
+One of the first algorithm used for approximating the integer square root of a positive integer n is known as "Hero's method", named after the first-century Greek mathematician Hero of Alexandria who gave the first description of the method. Hero's method can be obtained from Newton's method which came 16 centuries after.
+We approximate the square root of a number n by taking an initial guess x, an error e and repeatedly calculating a new approximate integer value x using: (x + n / x) / 2; we are finished when the previous x and the new x have an absolute difference less than e.
+We supply to a function (int_rac) a number n (positive integer) and a parameter guess (positive integer) which will be our initial x. For this kata the parameter 'e' is set to 1.
+Hero's algorithm is not always going to come to an exactly correct result! For instance: if n = 25 we get 5 but for n = 26 we also get 5. Nevertheless 5 is the integer square root of 26.
+The kata is to return the count of the progression of integer approximations that the algorithm makes.
+Reference:
+https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method
+Some examples:
+int_rac(25,1): follows a progression of [1,13,7,5] so our function should return 4.
+int_rac(125348,300): has a progression of [300,358,354] so our function should return 3.
+int_rac(125348981764,356243): has a progression of [356243,354053,354046] so our function should return 3.
+
+SOLUTIONS
+<?php
+function intRac($n, $guess) {
+    $e = 1;
+    
+    $i = 0;
+    $prevX = 0;
+    $newX = $guess;
+    
+    while(!$prevX || abs($prevX - $newX) >= $e) {
+      $i++;
+      $prevX = $newX;
+      $newX = floor(($prevX + $n / $prevX) / 2);
+    }  
+    return $i;
+}
+
+// function intRac($n, $guess) {
+//     $x = $guess; $cnt = 1; $newx = $x;
+//     while (true) {
+//         $newx = floor(($x + ($n / $x)) / 2);
+//         if (abs($x - $newx) < 1)
+//             return $cnt;
+//         $x = $newx;
+//         $cnt++;
+//     }
+// }
+
+// function intRac($n, $guess) {
+//     $c=0;
+//     do{
+//       $prev_guess=$guess;
+//       $guess=floor((($guess + ($n/$guess))/2));
+//        $c++;
+//     }while(abs($guess - $prev_guess) >= 1);
+//     return $c;
+// }
